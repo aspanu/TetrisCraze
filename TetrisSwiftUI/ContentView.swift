c8fb7ccf834976666de659391d08e94b5e86153b
Adrian Spanu <adri@nspanu.ca>
2024-07-29 13:38:13 +0900

    Initial Commit

---
+++ TetrisSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TetrisSwiftUI
+//
+//  Created by Adrian on 2024-07-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TetrisSwiftUI/TetrisSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetrisSwiftUIApp.swift
+//  TetrisSwiftUI
+//
+//  Created by Adrian on 2024-07-29.
+//
+
+import SwiftUI
+
+@main
+struct TetrisSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
